0d35a34e562118e359c82906709f973529036131
hjort <8545447+hjorthjort@users.noreply.github.com>
2025-06-05 14:40:05 -0400

    Unstoppable: fix

--- src/unstoppable/UnstoppableVault.sol
+++ src/unstoppable/UnstoppableVault.sol
@@ -81,8 +81,11 @@ contract UnstoppableVault is IERC3156FlashLender, ReentrancyGuard, Owned, ERC462
     {
         if (amount == 0) revert InvalidAmount(0); // fail early
         if (address(asset) != _token) revert UnsupportedCurrency(); // enforce ERC3156 requirement
+        // FIX(hjort): Remove the balance check.
+        /*
         uint256 balanceBefore = totalAssets();
-        if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); // enforce ERC4626 requirement
+        if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); //// enforce ERC4626 requirement
+        */
 
         // transfer tokens out + execute callback on receiver
         ERC20(_token).safeTransfer(address(receiver), amount);
